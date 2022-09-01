df6d764bcbc90301d383f0a6074ec3fccd4e75ce
Piumika Saranga <IT20260460@my.sliit.lk>
2022-09-01 15:12:16 +0530

    add category route

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 dotenv.config();
+const categoryRoute = require("./routes/category");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -16,6 +17,5 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend server is running!");
-  });
-  
+  console.log("Backend server is running!");
+});
--- Backend/routes/category.js
+++ Backend/routes/category.js
@@ -81,4 +81,5 @@ router.get("/", async (req, res) => {
   }
 });
 
+//return router
 module.exports = router;
