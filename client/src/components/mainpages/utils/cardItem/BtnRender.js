4283addc05269e7f81fb08b5437528ba8ce3e931
Pathiranage Vishwa <IT20260910@my.sliit.lk>
2022-09-16 02:45:01 +0530

    create card display page

---
+++ client/src/components/mainpages/utils/cardItem/BtnRender.js
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { GlobalState } from "../../../../GlobalState";
+
+function BtnRender({ book, deleteBook }) {
+  const state = useContext(GlobalState);
+  const [isAdmin] = state.userAPI.isAdmin;
+  const [isSeller] = state.userAPI.isSeller;
+  const [isBuyer] = state.userAPI.isBuyer;
+  const addCart = state.userAPI.addCart;
+  const [user] = state.userAPI.user;
+
+  return (
+    <div className="row_btn">
+      {isAdmin || user._id == book.seller_id ? (
+        <>
+          <Link
+            id="btn_buy"
+            to="#!"
+            onClick={() => deleteBook(book._id, book.images.public_id)}
+            // className="btn btn-lg text-uppercase  mb-5"
+          >
+            Delete
+          </Link>
+          <Link
+            id="btn_view"
+            to={`/edit_product/${book._id}`}
+            className="btn btn-lg  text-uppercase  mb-5"
+          >
+            Edit
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link
+            id="btn_buy"
+            to="#!"
+            onClick={() => addCart(book)}
+            // className="btn btn-lg  text-uppercase  mb-5"
+          >
+            Buy
+          </Link>
+          <Link
+            id="btn_view"
+            to={`/detail/${book._id}`}
+            className="btn btn-lg  text-uppercase  mb-5"
+          >
+            View
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default BtnRender;
---
+++ client/src/components/mainpages/utils/cardItem/CardItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import BtnRender from "./BtnRender";
+
+function CardItem({ book, isAdmin, deleteBook, handleCheck }) {
+  console.log(book);
+  console.log(isAdmin);
+  return (
+    <div className="product_card">
+      {isAdmin && (
+        <input
+          type="checkbox"
+          checked={book.checked}
+          onChange={() => handleCheck(book._id)}
+        />
+      )}
+      <img src={book.images.url} alt="" />
+
+      <div className="product_box">
+        <h5 title={book.title}>{book.title}</h5>
+        <span>${book.price}</span>
+        <p>{book.description}</p>
+      </div>
+
+      <BtnRender book={book} deleteBook={deleteBook} />
+    </div>
+  );
+}
+
+export default CardItem;
