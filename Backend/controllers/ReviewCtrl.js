3a2c2613f81a4042f7be8242c9e1fa21ec79eb17
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-09-21 18:29:58 +0530

    Add createReview method

--- Backend/controllers/ReviewCtrl.js
+++ Backend/controllers/ReviewCtrl.js
@@ -1,6 +1,23 @@
 const Reviews = require("../models/reviewModel");
 const Requests = require("../models/requestBookModel");
 
-const reviewBookCtrl = {}
+const reviewBookCtrl = {
+    createReviews: async (req, res) => {
+        try {
+            const { rate, date, review } = req.body;
+
+            const newReview = new Reviews({
+                rate,
+                date,
+                review,
+            });
+
+            await newReview.save();
+            res.json({ msg: "Submitted a Review" });
+        } catch (err) {
+            return res.status(500).json({ msg: err.message });
+        }
+    },
+}
 
 module.exports = reviewBookCtrl;
