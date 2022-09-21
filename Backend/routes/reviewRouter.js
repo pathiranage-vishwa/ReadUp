f6dc82f42ddbbdc1e18560e432750bdd1ec34ee1
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-09-21 19:06:52 +0530

    create post method in Router

--- Backend/routes/reviewRouter.js
+++ Backend/routes/reviewRouter.js
@@ -2,4 +2,8 @@ const reviewCtrl = require("../controllers/reviewCtrl");
 const auth = require("../middleware/auth");
 const router = require("express").Router();
 
+router
+    .route("/review")
+    .post(auth,reviewCtrl.createReviews);
+
 module.exports = router;
