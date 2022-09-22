ba2eada9ce78de44a7b43a62b4c21bd90bac1dae
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-09-22 20:48:41 +0530

    Connect backend and frontend

--- client/src/components/mainpages/Reviews/UpdateReview.js
+++ client/src/components/mainpages/Reviews/UpdateReview.js
@@ -2,3 +2,45 @@ import React, { useState, useContext, useEffect } from "react";
 import { GlobalState } from "../../../GlobalState";
 import axios from "axios";
 import swal from "sweetalert";
+
+function UpdateReview(){
+    const state = useContext(GlobalState);
+    const [token] = state.token;
+
+    const [id,setId] = useState();
+    const [rate,setRate] = useState();
+    const [date,setDate] = useState()
+    const [CommentReview,setCommentReview] = useState();
+
+    useEffect(()=>{
+        setId(localStorage.getItem("reid"));
+        setRate(localStorage.getItem("Rate"));
+        setDate(localStorage.getItem("Date"));
+        setCommentReview(localStorage.getItem("Comment"));
+    }, []);
+
+    const createReview = async (e)=>{
+        e.preventDefault();
+        const newReview = {
+            id,
+            rate,
+            date,
+            CommentReview,
+        };
+        axios
+            .put(`/api/review/${id}`, newReview)
+            .then(() =>{
+                swal("Done!", "Review Updated!!", "success");
+            })
+            .catch((err)=>{
+                alert(err);
+            });
+    };
+
+    return(
+        <div>
+
+        </div>
+    )
+
+}
