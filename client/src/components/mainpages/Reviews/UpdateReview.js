01fe07f420f9697eb85f14cc0ba182e2c64e03e2
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-09-22 20:51:37 +0530

    Design UI (UpdateReview)

--- client/src/components/mainpages/Reviews/UpdateReview.js
+++ client/src/components/mainpages/Reviews/UpdateReview.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { GlobalState } from "../../../GlobalState";
 import axios from "axios";
 import swal from "sweetalert";
+import UpdateReq from "../requests/Styles/UpdateReq.png";
 
 function UpdateReview(){
     const state = useContext(GlobalState);
@@ -38,8 +39,103 @@ function UpdateReview(){
     };
 
     return(
-        <div>
+        <div className="container">
+            <br/>
+            <div className="card">
+                <div className="card-body">
+                    <center>
+                        <h1>Update Request Book Details</h1>
+                        <hr/>
+                    </center>
 
+
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <img className="card-img-top" src={UpdateReq}  alt="Card image cap"/>
+                        </div>
+
+                        <div className="col-sm-6">
+                            <div className="card">
+                                <div className="card-body">
+                                    <form onSubmit={createRequest}>
+                                        <br/>
+                                        <label>Book Name: </label>
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                name="bookName"
+                                                defaultValue={bookName}
+                                                className="form-control form-control-lg"
+                                                onChange={(e) => {
+                                                    setBookName(e.target.value);
+                                                }}
+                                                required
+                                            />
+                                        </div>
+                                        <br/>
+
+                                        <label>Book Category : </label>
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                name="category"
+                                                defaultValue={category}
+                                                className="form-control form-control-lg"
+                                                onChange={(e) => {
+                                                    setCategory(e.target.value);
+                                                }}
+                                                required
+                                            />
+                                        </div>
+                                        <br/>
+
+                                        <label>Book Author : </label>
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                name="author"
+                                                defaultValue={author}
+                                                className="form-control form-control-lg"
+                                                onChange={(e) => {
+                                                    setAuthor(e.target.value);
+                                                }}
+                                                required
+                                            />
+                                        </div>
+                                        <br/>
+
+                                        <label>ISBN Number: </label>
+                                        <div className="form-group">
+                                            <input
+                                                type="text"
+                                                name="isbnNumber"
+                                                defaultValue={isbnNumber}
+                                                className="form-control form-control-lg"
+                                                onChange={(e) => {
+                                                    setIsbnNumber(e.target.value);
+                                                }}
+                                                required
+                                            />
+                                        </div>
+                                        <br/>
+
+
+                                        <div className="d-flex justify-content-end pt-3">
+                                            <button
+                                                className="btn btn-lg btn-success btn-login text-uppercase fw-bold mb-5"
+                                                type="submit"
+                                                style={{ height: "50px" }}
+                                            >
+                                                Update
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
