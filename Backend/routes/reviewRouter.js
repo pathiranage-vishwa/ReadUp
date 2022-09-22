210953dd690c2187fce41c6eef771d1809b7a462
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-09-22 20:33:19 +0530

    Add update Method in Router

--- Backend/routes/reviewRouter.js
+++ Backend/routes/reviewRouter.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const reviewCtrl = require("../controllers/reviewCtrl");
 const auth = require("../middleware/auth");
 
-
 router
     .route("/review")
     .post(auth,reviewCtrl.createReviews);
