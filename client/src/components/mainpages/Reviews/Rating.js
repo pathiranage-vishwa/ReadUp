d9ce35ef3de58215fb98861c166c99d129bf8c87
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-09-26 12:55:23 +0530

    Update File

--- client/src/components/mainpages/Reviews/Rating.js
+++ client/src/components/mainpages/Reviews/Rating.js
@@ -4,65 +4,66 @@ import PropTypes from 'prop-types'
 const Rating = ({ value, color }) => {
     return (
         <div className='rating'>
+            <i className="fa-solid fa-star-sharp"></i>
       <span>
         <i
-            style={{ color }}
-            className={
-                value >= 1
-                    ? 'fas fa-star'
-                    : value >= 0.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-            }
-        ></i>
+    style={{color}}
+    className={
+        value >= 1
+            ? 'fas fa-star'
+            : value >= 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+    }
+    />
       </span>
             <span>
         <i
-            style={{ color }}
-            className={
-                value >= 2
-                    ? 'fas fa-star'
-                    : value >= 1.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-            }
-        ></i>
+    style={{color}}
+    className={
+        value >= 2
+            ? 'fas fa-star'
+            : value >= 1.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+    }
+    />
       </span>
             <span>
         <i
-            style={{ color }}
-            className={
-                value >= 3
-                    ? 'fas fa-star'
-                    : value >= 2.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-            }
-        ></i>
+    style={{color}}
+    className={
+        value >= 3
+            ? 'fas fa-star'
+            : value >= 2.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+    }
+    />
       </span>
             <span>
         <i
-            style={{ color }}
-            className={
-                value >= 4
-                    ? 'fas fa-star'
-                    : value >= 3.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-            }
-        ></i>
+    style={{color}}
+    className={
+        value >= 4
+            ? 'fas fa-star'
+            : value >= 3.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+    }
+    />
       </span>
             <span>
         <i
-            style={{ color }}
-            className={
-                value >= 5
-                    ? 'fas fa-star'
-                    : value >= 4.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-            }
-        ></i>
+    style={{color}}
+    className={
+        value >= 5
+            ? 'fas fa-star'
+            : value >= 4.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+    }
+    />
       </span>
 
         </div>
@@ -75,7 +76,7 @@ Rating.defaultProps = {
 
 Rating.propTypes = {
     value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
+    // text: PropTypes.string.isRequired,
     color: PropTypes.string,
 }
 
