1057ba544293097365ce4a1ab607761fce04366d
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-09-26 17:27:35 +0530

    update

--- client/src/components/mainpages/Reviews/ManageReviews.js
+++ client/src/components/mainpages/Reviews/ManageReviews.js
@@ -41,36 +41,25 @@ export default function ManageReviews() {
                 <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Book Name</th>
-                    <th scope="col">Author</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">ISBN Number</th>
-                    <th scope="col">Status</th>
+                    <th scope="col">Rate</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Review</th>
                     <th scope="col">Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                {requests.map((data, index) => (
+                {reviews.map((data, index) => (
                     <tr key={index}>
                         <th scope="row">{index + 1}</th>
-                        <td>{data.bookName}</td>
-                        <td>{data.author}</td>
-                        <td>{data.category}</td>
-                        <td>{data.isbnNumber}</td>
-                        <td>{data.status}</td>
+                        <td>{data.rate}</td>
+                        <td>{data.date}</td>
+                        <td>{data.CommentReview}</td>
                         <td>
                             <a
-                                className="btn btn-warning"
-                                onClick={() => setData(data._id)}
-                            >
-                                &nbsp;Accept
-                            </a>
-                            &nbsp;
-                            <a
                                 className="btn btn-danger"
-                                onClick={() => handleDelete(data._id)}
+                                onClick={() => deleteReview(data._id)}
                             >
-                                &nbsp;Decline
+                                &nbsp;Delete
                             </a>
                         </td>
                     </tr>
