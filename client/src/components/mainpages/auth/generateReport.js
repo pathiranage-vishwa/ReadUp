18e58c2f70828da857482e81b10c7074b87529ed
Ravindu Yasith <IT20236014@my.sliit.lk>
2022-10-01 22:13:42 +0530

    User_Report_6

--- client/src/components/mainpages/auth/generateReport.js
+++ client/src/components/mainpages/auth/generateReport.js
@@ -23,21 +23,49 @@ const GetReport = () => {
   }, []);
 
   
-  return (
-    <div>
-      <div className="container mb-4 mt-4 p-3">
-        <div className="row">
+//   return (
+//     <div>
+//       <div className="container mb-4 mt-4 p-3">
+//         <br/><br/><br/><br/><br/><br/><br/><br/>
+//         <div className="row">
          
-            <button
-              className="btn btn-primary"
-              onClick={() => generatePDF(profile)}
-            >
-              Generate monthly report
-            </button>
+//             <button
+//               className="btn btn-primary"
+//               onClick={() => generatePDF(profile)}
+//             >
+//               Generate monthly report
+//             </button>
           
+//         </div>
+//       </div>
+     
+//     </div>
+//   );
+return (
+    <div className="container-fluid ps-md-0">
+      <div className="row g-0">
+        <div className="d-none d-md-flex col-md-4 col-lg-6 logimage"></div>
+        <div className="col-md-8 col-lg-5">
+          <div className="login d-flex align-items-center py-5">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-9 col-lg-8 mx-auto">
+                  <h3 className="log-heading mb-4">Generate User Report</h3>
+                    <div className="d-grid">
+                      <button
+                        className="btn btn-lg btn-success btn-login text-uppercase fw-bold mb-2"
+                        onClick={() => generatePDF(profile)}
+                        style={{ height: "50px" }}
+                      >
+                        Sign in
+                      </button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-     
     </div>
   );
 };
